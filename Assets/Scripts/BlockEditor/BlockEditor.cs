4f6612df9f93bc2163c90f663ecd95041df511d0
Tobias Kiehnlein <tobias@kiehnlein.com>
2020-10-03 16:54:20 +0200

    Improve Editor

---
+++ Assets/Scripts/BlockEditor/BlockEditor.cs
@@ -0,0 +1,94 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace BlockEditor
+{
+    public class BlockEditor : MonoBehaviour
+    {
+        [SerializeField] private GameObject[] listElements;
+        [SerializeField] private float distance = 3;
+        [SerializeField] private float scrollBlockSize = 3;
+        [SerializeField] private float blockScrollSpeed = 5;
+
+        private Vector3 _destinationPosition;
+        private bool _dragging;
+        private Vector3 _initialOffset;
+        private Vector3 _prevMousePos;
+        private List<SelectableElement> _selectableElements;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _selectableElements = new List<SelectableElement>();
+            var localTransform = transform;
+            var localPosition = localTransform.position;
+            _destinationPosition = localPosition;
+            float offset = 0;
+
+            foreach (var listElement in listElements)
+            {
+                Vector3 position = new Vector3(offset + localPosition.x, localPosition.y, localPosition.z);
+                GameObject go = Instantiate(listElement, position, Quaternion.identity);
+                go.transform.parent = localTransform;
+                offset += distance;
+                SelectableElement se = go.GetComponent<SelectableElement>();
+                if (se != null)
+                    _selectableElements.Add(se);
+            }
+        }
+
+        public void ScrollRightBlockwise()
+        {
+            ScrollBlockwise(-scrollBlockSize);
+        }
+
+        public void ScrollLeftBlockwise()
+        {
+            ScrollBlockwise(scrollBlockSize);
+        }
+
+        private void ScrollBlockwise(float amount)
+        {
+            _selectableElements.ForEach(element => element.ScrollBy(amount));
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (!_dragging && transform.position.Equals(_destinationPosition))
+            {
+                transform.position = Vector3.Lerp(transform.position, _destinationPosition, blockScrollSpeed * Time.deltaTime);
+            }
+        }
+
+        // private void OnMouseDown()
+        // {
+        //     _dragging = true;
+        //
+        //     Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 3);
+        //     Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        //     objPosition.y = transform.position.y;
+        //
+        //     _initialOffset = transform.position - objPosition;
+        //     _prevMousePos = mousePosition;
+        // }
+        //
+        // private void OnMouseUp()
+        // {
+        //     _dragging = false;
+        // }
+        //
+        // void OnMouseDrag()
+        // {
+        //     Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 3);
+        //     Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        //     objPosition.y = transform.position.y;
+        //
+        //     transform.position = objPosition + _initialOffset;
+        //     _destinationPosition = objPosition + _initialOffset;
+        //     
+        //     
+        // }
+    }
+}
---
+++ Assets/Scripts/BlockEditor/SelectableElement.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+
+namespace BlockEditor
+{
+    public class SelectableElement : MonoBehaviour
+    {
+        public bool isPlaced = false;
+        [SerializeField] private float blockScrollSpeed = 5;
+        [SerializeField] private float snapBackDistance = 5;
+        [SerializeField] private Vector3 previewScale = Vector3.one * 0.5f;
+        [SerializeField] private float scalingSpeed = 4;
+
+        private bool _dragging;
+        private Vector3 _destinationPosition;
+        private Vector3 _initialPosition;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _initialPosition = transform.position;
+            _destinationPosition = transform.position;
+            transform.localScale = previewScale;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (!_dragging && !transform.position.Equals(_destinationPosition))
+            {
+                transform.position = Vector3.Lerp(transform.position, _destinationPosition, blockScrollSpeed * Time.deltaTime);
+            }
+
+            if (!_dragging && !isPlaced)
+            {
+                //TODO Lerp to small
+                if (transform.localScale != previewScale)
+                {
+                    transform.localScale = Vector3.Lerp(transform.localScale, previewScale, scalingSpeed * Time.deltaTime);
+                }
+            }
+            else
+            {
+                //TODO Lerp to large
+                if (transform.localScale != Vector3.one)
+                {
+                    transform.localScale = Vector3.Lerp(transform.localScale, Vector3.one, scalingSpeed * Time.deltaTime);
+                }
+            }
+        }
+
+        private void OnMouseDown()
+        {
+            _dragging = true;
+
+            _initialPosition = transform.position;
+        }
+
+        private void OnMouseUp()
+        {
+            _dragging = false;
+            // if (!((_initalPosition - transform.position).y < snapBackDistance) || isPlaced) return;
+
+            if ((_initialPosition - transform.position).magnitude > snapBackDistance || isPlaced)
+            {
+                isPlaced = true;
+                return;
+            }
+
+            _destinationPosition = _initialPosition;
+        }
+
+        void OnMouseDrag()
+        {
+            Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 3);
+            Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+            transform.position = objPosition;
+            _destinationPosition = objPosition;
+        }
+
+        public void ScrollBy(float amount)
+        {
+            if (!isPlaced)
+                _destinationPosition.x += amount;
+        }
+    }
+}
