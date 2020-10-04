a4311c4dbdc13db16e8f58613cba6a0e90f63f57
Raphael Kölz <raphael.koelz@gmx.net>
2020-10-04 17:40:54 +0200

    Added Title Screen

---
+++ Assets/Scripts/Game/SceneManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+    public void ChangeScene(int sceneId)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneId);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
