9bf08f55f56fd15914bd43e52419a99c31c658f8
Raphael Kölz <raphael.koelz@gmx.net>
2020-10-04 07:15:57 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/Audio/AudioCue.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Fictions/Audio Cue")]
+public class AudioCue : ScriptableObject
+{
+    [SerializeField] private List<AudioClip> _clips = new List<AudioClip>();
+
+    [SerializeField, HideInInspector] private float _minVolume; 
+    [SerializeField, HideInInspector] private float _maxVolume;
+    
+    [SerializeField, HideInInspector] private float _minPitch; 
+    [SerializeField, HideInInspector] private float _maxPitch;   
+    
+    [ShowInInspector]
+    public float MinVolume
+    {
+        get => _minVolume;
+        set => _minVolume = Mathf.Clamp(value, 0, _maxVolume);
+    }
+
+    [ShowInInspector]
+    public float MaxVolume
+    {
+        get => _maxVolume;
+        set => _maxVolume = Mathf.Clamp(value, _minVolume, 1);
+    }
+    
+    
+    [ShowInInspector]
+    public float MinPitch
+    {
+        get => _minPitch;
+        set => _minPitch = Mathf.Clamp(value, -3, _maxPitch);
+    }  
+    
+    [ShowInInspector]
+    public float MaxPitch
+    {
+        get => _maxPitch;
+        set => _maxPitch = Mathf.Clamp(value, _minPitch, 3);
+    }     
+  
+
+    public void Play(AudioSource audioSource)
+    {
+        var volume = Random.Range(_minVolume, _maxVolume);
+        var pitch = Random.Range(_minPitch, _maxPitch);
+        
+        audioSource.volume = volume;
+        audioSource.pitch = pitch;
+
+        var clip = _clips[Random.Range(0, _clips.Count)];
+        audioSource.PlayOneShot(clip);
+    }
+}
---
+++ Assets/Scripts/Audio/AudioPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioPlayer : MonoBehaviour
+{
+    [SerializeField] private AudioCue _audioCue;
+    [SerializeField] private float _delayInSeconds;
+
+    private void OnEnable()
+    {
+        StartCoroutine(PlaySound());
+    }
+
+    private IEnumerator PlaySound()
+    {
+        yield return new WaitForSeconds(_delayInSeconds);
+        _audioCue.Play(GetComponent<AudioSource>());
+    }
+}
